021186f9143bb9da6e2b49671affe70d80310b3a
Büşra Özata <busraozata1998@gmail.com>
2023-03-23 18:25:00 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,98 @@
+const menuToggle = document.querySelector("#toggle-btn");
+const navigation = document.querySelector(".mobile-bottom");
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+
+  if (menuToggle.innerHTML === `<i class="fa-solid fa-xmark"></i>`) {
+    menuToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    menuToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+};
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $(".header-inner").addClass("sticky");
+  } else {
+    $(".header-inner").removeClass("sticky");
+  }
+});
+$('a[href="#search"]').click(function () {
+  event.preventDefault();
+  $("#search-box").addClass("-open");
+  setTimeout(function () {
+    inputSearch.focus();
+  }, 800);
+});
+
+$('a[href="#close"]').click(function () {
+  event.preventDefault();
+  $("#search-box").removeClass("-open");
+});
+
+$(document).keyup(function (e) {
+  if (e.keyCode == 27) {
+    // escape key maps to keycode `27`
+    $("#search-box").removeClass("-open");
+  }
+});
 var swiper = new Swiper(".mySwiperBanner", {});
+var swiperTransfer = new Swiper(".mySwiperTransfer", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
+
 AOS.init();
 var a = 0;
 $(window).scroll(function () {
-    var oTop = $("#counter-box").offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-        $(".counter").each(function () {
-            var $this = $(this),
-                countTo = $this.attr("data-number");
-            $({
-                countNum: $this.text()
-            }).animate(
-                {
-                    countNum: countTo
-                },
+  var oTop = $("#counter-box").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".counter").each(function () {
+      var $this = $(this),
+        countTo = $this.attr("data-number");
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
 
-                {
-                    duration: 1900,
-                    easing: "swing",
-                    step: function () {
-                        //$this.text(Math.ceil(this.countNum));
-                        $this.text(
-                            Math.ceil(this.countNum).toLocaleString("en")
-                        );
-                    },
-                    complete: function () {
-                        $this.text(
-                            Math.ceil(this.countNum).toLocaleString("en")
-                        );
-                        //alert('finished');
-                    }
-                }
-            );
-        });
-        a = 1;
-    }
+        {
+          duration: 1900,
+          easing: "swing",
+          step: function () {
+            //$this.text(Math.ceil(this.countNum));
+            $this.text(Math.ceil(this.countNum).toLocaleString("en"));
+          },
+          complete: function () {
+            $this.text(Math.ceil(this.countNum).toLocaleString("en"));
+            //alert('finished');
+          },
+        }
+      );
+    });
+    a = 1;
+  }
 });
